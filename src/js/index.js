4f98bb1c7315b942d1a1939463571cedbc6c2721
toilamanh <nguyenxuanmanh030929@gmail.com>
2025-01-15 17:22:47 +0700

    Add javascript

---
+++ src/js/index.js
@@ -0,0 +1,72 @@
+// buổi 1
+// Javascript: biến(let, var, const), kiểu dữ liệu ngầm định
+// cấu trúc rẽ nhánh if else, toán tử 3 ngôi
+
+// printf("Hello") -> Javascript; console.log("Hello")
+// Javascrit là ngôn ngữ hướng thủ tục -> lập trình hàm
+// console.log("Dat Hello world javascript");
+
+// kiểu dữ liệu ngầm định, tức là em không phải khai báo nó tường minh
+// float a = 1;
+
+// var(function), let (block) tự suy ra kiểu dữ liệu khác nhau phạm vị truy cập
+// const tự suy ra kiểu dữ liệu nhưng nó là biến hằng (tức là chỉ được khởi tạo giá trị 1 lần)
+
+// kieu du lieu ten bien = gia tri
+// let/var/const ten_bien = gia tri -> Javascript
+// toan tu so hoc: +, -, *, /, %, ** (lũy thừa)
+// toán tử logic: && (Và), ||(Hoặc), !(phủ định)
+// toán tử so sánh: >=, <=, ==, !=... -> boolean
+// ++a (tiền tố), a++ (hậu tố) tương tự với -
+// ++a trả về giá trị sau tăng, a++ tăng lên 1 và trả về giá trị trước tăng
+// 2^2 = 4, 2^3 = 2 * 2 * 2 = 8
+
+// let a = 31;
+// if (a >= 12) {
+//   if (a < 30) {
+//     if (a == 29) {
+//       console.log("Sucsess");
+//     }
+//   } else {
+//     console.log("Hello");
+//   }
+// } else if (a > 30 && a <= 90) console.log("NAm trong khoang 30 - 90");
+
+// let c = 11;
+// let kq = c > 12 ? true : false;
+// console.log(kq);
+
+// for, white, do-while, for-in, for of, mang
+// for (int i = 1; i < 10; i++) {
+
+// }
+
+// for (let i = 1; i <= 10; i = i + 1) {
+//   console.log(i);
+// }
+// console.log("///");
+
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+// let a = 3;
+// do {
+//   console.log("ANh manh");
+//   a--;
+// } while (a >= 2);
+
+// let a = 1;
+// while (a <= 10) {
+//   console.log(a);
+//   a++;
+// }
+
+// bang cuu chuong
+// for (let i = 2; i <= 10; i++) {
+//   console.log("Bang ", i);
+
+//   for (let j = 1; j <= 10; j++) {
+//     console.log(i, "*", j, "=", i * j);
+//   }
+// }
